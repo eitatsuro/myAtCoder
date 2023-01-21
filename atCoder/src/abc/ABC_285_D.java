65978843d8fa7086bfcea55b3907adbaeab0d048
eitatsuro <tatsu@TABLET-4RM89ESH>
2023-01-21 23:58:05 +0900

    Cはやってない（クラス名の末尾Cは間違いで正しくはD）ので削除、Dは途中

--- atCoder/src/abc/ABC_285_D.java
+++ atCoder/src/abc/ABC_285_D.java
@@ -2,7 +2,7 @@ package abc;
 
 import java.util.Scanner;
 
-public class ABC_285_C {
+public class ABC_285_D {
 	public static void main(String[] args) {
 		try (Scanner sc = new Scanner(System.in)) {
 			int N = sc.nextInt();
